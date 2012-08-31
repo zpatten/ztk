ba9d8d4a4d84263bd6ae319fc090eda51438b1c4
Zachary Patten <zim931@gmail.com>
2012-08-31 16:42:46 -0700

    tweaked logging message

--- lib/ztk/logger.rb
+++ lib/ztk/logger.rb
@@ -56,7 +56,7 @@ module ZTK
       msg = (block && block.call)
       (msg.nil? || msg.strip.empty?) and return
       message = [message, progname, msg].delete_if{ |i| i.nil? }.join(": ")
-      message = "%19s.%06d|%5d|%5s|%s%s\n" % [Time.now.utc.strftime("%Y-%m-%d|%H:%M:%S"), Time.now.utc.usec, Process.pid, SEVERITIES[severity], called_by, message]
+      message = "%19s.%06d|%05d|%5s|%s%s\n" % [Time.now.utc.strftime("%Y-%m-%d|%H:%M:%S"), Time.now.utc.usec, Process.pid, SEVERITIES[severity], called_by, message]
 
       @logdev.write(message)
 
