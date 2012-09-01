1a3ef375ec5a6ffe82962e64ceb4b16cdc9a3373
Zachary Patten <zim931@gmail.com>
2012-09-01 14:36:54 -0700

    extended ZTK::TCPSocketCheck spec

--- spec/ztk/tcp_socket_check_spec.rb
+++ spec/ztk/tcp_socket_check_spec.rb
@@ -30,14 +30,14 @@ describe ZTK::TCPSocketCheck do
       subject.should be ZTK::TCPSocketCheck
     end
 
-    describe "default config" do
+    describe "config" do
 
       it "should throw an exception if the host is not specified" do
-        lambda{ subject.new(:port => 22) }.should raise_error ZTK::TCPSocketCheckError, "You must supply a host!"
+        lambda{ subject.new(:port => 22).ready? }.should raise_error ZTK::TCPSocketCheckError, "You must supply a host!"
       end
 
       it "should throw an exception if the port is not specified" do
-        lambda{ subject.new(:host => "127.0.0.1") }.should raise_error ZTK::TCPSocketCheckError, "You must supply a port!"
+        lambda{ subject.new(:host => "127.0.0.1").ready? }.should raise_error ZTK::TCPSocketCheckError, "You must supply a port!"
       end
 
     end
@@ -46,14 +46,68 @@ describe ZTK::TCPSocketCheck do
 
   describe "behaviour" do
 
-    it "should signal ready? is true on remote read test to github.com:22" do
-      tcp_check = subject.new(:host => "github.com", :port => 22)
-      tcp_check.ready?.should == true
+    describe "ready?" do
+
+      describe "read check" do
+
+        it "should return true on a remote read check to github.com:22" do
+          tcp_check = subject.new(:host => "github.com", :port => 22)
+          tcp_check.ready?.should == true
+        end
+
+        it "should return false on a remote read check to 127.0.0.1:1" do
+          tcp_check = subject.new(:host => "127.0.0.1", :port => 1, :timeout => 3)
+          tcp_check.ready?.should == false
+        end
+
+      end
+
+      describe "write check" do
+
+        it "should return true on a remote write check to www.google.com:80" do
+          tcp_check = subject.new(:host => "www.google.com", :port => 80, :data => "GET")
+          tcp_check.ready?.should == true
+        end
+
+        it "should return false on a remote write check to 127.0.0.1:1" do
+          tcp_check = subject.new(:host => "127.0.0.1", :port => 1, :data => "GET", :timeout => 3)
+          tcp_check.ready?.should == false
+        end
+
+      end
+
     end
 
-    it "should signal ready? is true on remote write test to www.google.com:80" do
-      tcp_check = subject.new(:host => "www.google.com", :port => 80, :data => "GET")
-      tcp_check.ready?.should == true
+    describe "wait" do
+
+      describe "read check" do
+
+        it "should timeout and should return false on a read check to 127.0.0.1:1" do
+          tcp_check = subject.new(:host => "127.0.0.1", :port => 1, :wait => 3)
+          tcp_check.wait.should == false
+        end
+
+        it "should not timeout and should return true on a read check to github.com:22" do
+          tcp_check = subject.new(:host => "github.com", :port => 22, :wait => 3)
+          tcp_check.wait.should == true
+        end
+
+      end
+
+      describe "write check" do
+
+        it "should timeout and should return false on a write check to 127.0.0.1:1" do
+          tcp_check = subject.new(:host => "127.0.0.1", :port => 1, :data => "GET", :wait => 3)
+          tcp_check.wait.should == false
+        end
+
+        it "should not timeout and should return true on a write check to www.google.com:80" do
+          tcp_check = subject.new(:host => "www.google.com", :port => 80, :data => "GET", :wait => 3)
+          tcp_check.wait.should == true
+        end
+
+      end
+
     end
 
   end
