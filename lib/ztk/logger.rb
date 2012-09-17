4f00786def6d7aee6317029c48030178b90bc01b
Zachary Patten <zim931@gmail.com>
2012-09-16 23:08:15 -0700

    replace delete_if with compact

--- lib/ztk/logger.rb
+++ lib/ztk/logger.rb
@@ -62,7 +62,7 @@ module ZTK
       called_by = parse_caller(caller[1])
       msg = (block && block.call)
       (msg.nil? || msg.strip.empty?) and return
-      message = [message, progname, msg].delete_if{ |i| i.nil? }.join(": ")
+      message = [message, progname, msg].flatten.compact.join(": ")
       message = "%19s.%06d|%05d|%5s|%s%s\n" % [Time.now.utc.strftime("%Y-%m-%d|%H:%M:%S"), Time.now.utc.usec, Process.pid, SEVERITIES[severity], called_by, message]
 
       @logdev.write(message)
