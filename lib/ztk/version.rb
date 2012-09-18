dc43dae3bde1fd172c87fcea9f4a4c26b8ed55a3
Zachary Patten <zim931@gmail.com>
2012-09-17 22:16:36 -0700

    version bump

--- lib/ztk/version.rb
+++ lib/ztk/version.rb
@@ -19,5 +19,5 @@
 ################################################################################
 
 module ZTK
-  VERSION = "0.0.8" unless const_defined?(:VERSION)
+  VERSION = "0.0.9" unless const_defined?(:VERSION)
 end
