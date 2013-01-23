5f791af055d208983f290b13986bfbd2380da019
zpatten <zpatten@jovelabs.com>
2013-01-23 01:27:09 -0800

    version bump

--- lib/ztk/version.rb
+++ lib/ztk/version.rb
@@ -19,5 +19,5 @@
 ################################################################################
 
 module ZTK
-  VERSION = "0.1.0" unless const_defined?(:VERSION)
+  VERSION = "0.1.1" unless const_defined?(:VERSION)
 end
