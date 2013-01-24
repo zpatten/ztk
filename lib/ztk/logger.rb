57ba9f1d42ac70be9ee870a76668121869698546
zpatten <zpatten@jovelabs.com>
2013-01-24 15:32:30 -0800

    flush the log device if it allows it

--- lib/ztk/logger.rb
+++ lib/ztk/logger.rb
@@ -92,6 +92,7 @@ module ZTK
       message = "%19s.%06d|%05d|%5s|%60s%s\n" % [Time.now.utc.strftime("%Y-%m-%d|%H:%M:%S"), Time.now.utc.usec, Process.pid, SEVERITIES[severity], called_by, message]
 
       @logdev.write(message)
+      @logdev.respond_to?(:flush) and @logdev.flush
 
       true
     end
