8842727564913bcd8bba3267912b2a4a0bf1b0fe
zpatten <zpatten@jovelabs.com>
2013-01-26 22:10:30 -0800

    add belongs to

--- lib/ztk/dsl/core/relations.rb
+++ lib/ztk/dsl/core/relations.rb
@@ -20,11 +20,13 @@
 
 module ZTK::DSL::Core
   module Relations
+    autoload :BelongsTo, "ztk/dsl/core/relations/belongs_to"
     autoload :HasMany, "ztk/dsl/core/relations/has_many"
 
     def self.included(base)
       base.class_eval do
         base.send(:extend, ZTK::DSL::Core::Relations::ClassMethods)
+        base.send(:include, ZTK::DSL::Core::Relations::BelongsTo)
         base.send(:include, ZTK::DSL::Core::Relations::HasMany)
       end
     end
