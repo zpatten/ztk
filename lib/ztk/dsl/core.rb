ce7a588c31a666b68eb8e0b10a3f2dc7693ed8e8
zpatten <zpatten@jovelabs.com>
2013-01-26 20:59:27 -0800

    need some helpers

--- lib/ztk/dsl/core.rb
+++ lib/ztk/dsl/core.rb
@@ -15,11 +15,67 @@ module ZTK::DSL
 
     module DualMethods
 
-      def singularize(string)
+      def singularize(str)
+        string = str.to_s
         if string =~ /s$/
           string = string[0..-2]
         end
         string
+      end
+
+      def camelize(str)
+        string = str.to_s
+        parts = string.split("_")
+        string = parts.collect{ |part| part.capitalize }.join
+        string
+      end
+
+      def classify(str)
+        string = str.to_s
+        camelize(singularize(string))
+      end
+
+      # Ruby 1.9 introduces an inherit argument for Module#const_get and
+      # #const_defined? and changes their default behavior.
+      if Module.method(:const_get).arity == 1
+        # Tries to find a constant with the name specified in the argument string:
+        #
+        #   "Module".constantize     # => Module
+        #   "Test::Unit".constantize # => Test::Unit
+        #
+        # The name is assumed to be the one of a top-level constant, no matter whether
+        # it starts with "::" or not. No lexical context is taken into account:
+        #
+        #   C = 'outside'
+        #   module M
+        #     C = 'inside'
+        #     C               # => 'inside'
+        #     "C".constantize # => 'outside', same as ::C
+        #   end
+        #
+        # NameError is raised when the name is not in CamelCase or the constant is
+        # unknown.
+        def constantize(camel_cased_word)
+          names = camel_cased_word.split('::')
+          names.shift if names.empty? || names.first.empty?
+
+          constant = Object
+          names.each do |name|
+            constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+          end
+          constant
+        end
+      else
+        def constantize(camel_cased_word) #:nodoc:
+          names = camel_cased_word.split('::')
+          names.shift if names.empty? || names.first.empty?
+
+          constant = Object
+          names.each do |name|
+            constant = constant.const_defined?(name, false) ? constant.const_get(name) : constant.const_missing(name)
+          end
+          constant
+        end
       end
 
     end
