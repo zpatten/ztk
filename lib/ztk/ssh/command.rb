e0283133e223bec8af6ea4315e53b170e53cbd44
Zachary Patten <zachary@jovelabs.com>
2013-04-26 19:43:10 -0700

    make verbosity level smarter; if LOG_LEVEL=DEBUG then be verbose; otherwise be quiet

--- lib/ztk/ssh/command.rb
+++ lib/ztk/ssh/command.rb
@@ -24,8 +24,10 @@ module ZTK
 
       # Builds our SSH console command.
       def console_command
+        verbosity = ((ENV['LOG_LEVEL'].upcase == "DEBUG") ? '-vv' : '-q')
+
         command = [ "/usr/bin/env ssh" ]
-        command << [ "-q" ]
+        command << [ verbosity ]
         command << [ "-x" ]
         command << [ "-a" ]
         command << [ "-o", "UserKnownHostsFile=/dev/null" ]
@@ -35,7 +37,6 @@ module ZTK
         command << [ "-o", %(ProxyCommand="#{proxy_command}") ] if config.proxy_host_name
         command << [ "-i", config.keys ] if config.keys
         command << [ "-p", config.port ] if config.port
-        # command << [ "-vv" ]
         command << "#{config.user}@#{config.host_name}"
         command = command.flatten.compact.join(' ')
         config.ui.logger.debug { "console_command(#{command.inspect})" }
@@ -47,8 +48,10 @@ module ZTK
         !config.proxy_user and log_and_raise(SSHError, "You must specify an proxy user in order to SSH proxy.")
         !config.proxy_host_name and log_and_raise(SSHError, "You must specify an proxy host_name in order to SSH proxy.")
 
+        verbosity = ((ENV['LOG_LEVEL'].upcase == "DEBUG") ? '-vv' : '-q')
+
         command = ["/usr/bin/env ssh"]
-        command << [ "-q" ]
+        command << [ verbosity ]
         command << [ "-x" ]
         command << [ "-a" ]
         command << [ "-o", "UserKnownHostsFile=/dev/null" ]
@@ -57,7 +60,6 @@ module ZTK
         command << [ "-o", "ServerAliveInterval=60" ]
         command << [ "-i", config.proxy_keys ] if config.proxy_keys
         command << [ "-p", config.proxy_port ] if config.proxy_port
-        # command << [ "-vv" ]
         command << "#{config.proxy_user}@#{config.proxy_host_name}"
         command << "'/usr/bin/env nc %h %p'"
         command = command.flatten.compact.join(' ')
