b45f792896eb55e617df637a02f380da0cbbe708
Zachary Patten <zachary@jovelabs.com>
2013-05-04 11:52:01 -0700

    set -x

--- spec/support/before_install.sh
+++ spec/support/before_install.sh
@@ -18,11 +18,14 @@
 #
 ################################################################################
 #!/bin/bash
+set -x
+
 apt-get -qq update
+ssh-keygen -N ''
+
 # cat <<EOF | tee /usr/sbin/policy-rc.d
 # #!/bin/sh
 # exit 101
 # EOF
 # chmod 755 /usr/sbin/policy-rc.d
 # apt-get -qq install lxc
-ssh-keygen -N ''
