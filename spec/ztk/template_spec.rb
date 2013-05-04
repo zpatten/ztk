50e33d392858c2a517b25e10cb4e28e70192784b
Zachary Patten <zachary@jovelabs.com>
2013-05-04 11:44:38 -0700

    update ZTK::Template specs

--- spec/ztk/template_spec.rb
+++ spec/ztk/template_spec.rb
@@ -32,13 +32,47 @@ describe ZTK::Template do
 
   end
 
-  describe "behaviour" do
+  describe "methods" do
 
-    it "should render the template with the supplied context" do
-      template_file = File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "test-template.txt.erb"))
-      context = { :test_variable => "Hello World" }
-      output = subject.render(template_file, context)
-      output.should == "Hello World"
+    describe "do_not_edit_notice" do
+
+      it "should render the notice with no options" do
+        result = subject.do_not_edit_notice
+        result.should =~ /WARNING: AUTOMATICALLY GENERATED FILE; DO NOT EDIT!/
+        result.should =~ /Generated @/
+        result.should =~ /#/
+      end
+
+      it "should render the notice with our message inside" do
+        message = "Hello World"
+        result = subject.do_not_edit_notice(:message => message)
+        result.should =~ /WARNING: AUTOMATICALLY GENERATED FILE; DO NOT EDIT!/
+        result.should =~ /Generated @/
+        result.should =~ /#/
+        result.should =~ /#{message}/
+      end
+
+      it "should allow us to change the comment character" do
+        message = "Hello World"
+        char = "ZZ"
+        result = subject.do_not_edit_notice(:message => message, :char => char)
+        result.should =~ /WARNING: AUTOMATICALLY GENERATED FILE; DO NOT EDIT!/
+        result.should =~ /Generated @/
+        result.should =~ /#{message}/
+        result.should =~ /#{char}/
+      end
+
+    end
+
+    describe "render" do
+
+      it "should render the template with the supplied context" do
+        template_file = File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "test-template.txt.erb"))
+        context = { :test_variable => "Hello World" }
+        output = subject.render(template_file, context)
+        output.should == "Hello World"
+      end
+
     end
 
   end
