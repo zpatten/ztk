99043f17ded320d71bc3c198fe911f2927f4b9cc
Zachary Patten <zachary@jovelabs.com>
2013-05-13 11:27:18 -0700

    allow recursive downloads

--- lib/ztk/ssh/download.rb
+++ lib/ztk/ssh/download.rb
@@ -8,6 +8,11 @@ module ZTK
       #
       # @param [String] remote The remote file/path you with to download from.
       # @param [String] local The local file/path you wish to download to.
+      # @param [Hash] options An optional hash of options.
+      # @option options [Boolean] :recursive Whether or not to recursively
+      #   download files and directories.  By default this looks at the local
+      #   target and if it is a directory this is set to *true* otherwise it
+      #   will default to *false*.
       #
       # @example Download a file:
       #   $logger = ZTK::Logger.new(STDOUT)
@@ -19,12 +24,13 @@ module ZTK
       #   local = File.expand_path(File.join("/tmp", "id_rsa.pub"))
       #   remote = File.expand_path(File.join(ENV["HOME"], ".ssh", "id_rsa.pub"))
       #   ssh.download(remote, local)
-      def download(remote, local)
+      def download(remote, local, options={})
         config.ui.logger.debug { "config=#{config.send(:table).inspect}" }
         config.ui.logger.info { "download(#{remote.inspect}, #{local.inspect})" }
+        options = {:recursive => ::File.directory?(local) }.merge(options)
 
         ZTK::RescueRetry.try(:tries => 3, :on => EOFError, :on_retry => method(:on_retry)) do
-          sftp.download!(remote.to_s, local.to_s) do |event, downloader, *args|
+          sftp.download!(remote.to_s, local.to_s, options) do |event, downloader, *args|
             case event
             when :open
               config.ui.logger.debug { "download(#{args[0].remote} -> #{args[0].local})" }
