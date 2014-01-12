087fdf759be01bab9e436a82768b9ea5fcafe587
Zachary Patten <zachary@jovelabs.com>
2014-01-11 20:37:26 -0800

    rescue network unreachable

--- lib/ztk/tcp_socket_check.rb
+++ lib/ztk/tcp_socket_check.rb
@@ -98,7 +98,7 @@ module ZTK
         ((IO.select(nil, [socket], nil, config.timeout) && socket.write(config.data)) ? true : false)
       end
 
-    rescue Errno::ETIMEDOUT, Errno::ECONNREFUSED, Errno::ECONNRESET, Errno::EHOSTUNREACH, Errno::EHOSTDOWN => e
+    rescue Errno::ETIMEDOUT, Errno::ECONNREFUSED, Errno::ECONNRESET, Errno::EHOSTUNREACH, Errno::EHOSTDOWN, Errno::ENETUNREACH => e
       config.ui.logger.debug { "#{config.host}:#{config.port} - #{e.message}" }
       false
     ensure
