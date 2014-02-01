b02376eae5767e835b95a213654d298dc02c71ba
Zachary Patten <zachary@jovelabs.com>
2014-02-01 00:45:16 -0800

    refactor rake docs helper

--- lib/ztk/rake/docs.rb
+++ lib/ztk/rake/docs.rb
@@ -5,25 +5,31 @@ GEM_NAME    = File.basename(Dir.pwd)
 VENDOR_PATH = File.expand_path(File.join(Dir.pwd, "vendor"))
 DOC_PATH    = File.join(VENDOR_PATH, "docs")
 
-namespace :doc do
+namespace :docs do
 
-  YARD::Rake::YardocTask.new(:pages) do |t|
-    if !File.exists?(DOC_PATH)
-      remote = %x(git remote -v | grep 'origin').split[1].strip
-
-      FileUtils.mkdir_p(VENDOR_PATH)
-      Dir.chdir(VENDOR_PATH) do
-        system(%(git clone --branch gh-pages #{remote} #{DOC_PATH}))
-      end
-    end
-
+  YARD::Rake::YardocTask.new(:generate) do |t|
     t.options = ['--verbose', '-o', DOC_PATH]
   end
 
-  namespace :pages do
+  namespace :ghpages do
 
-    desc 'Generate and publish YARD Documentation to GitHub pages'
-    task :publish => ['doc:pages'] do
+    desc "Clone GitHub pages under vendor"
+    task :clone do
+      if !File.exists?(DOC_PATH)
+        remote = %x(git remote -v | grep 'origin').split[1].strip
+
+        FileUtils.mkdir_p(VENDOR_PATH)
+        Dir.chdir(VENDOR_PATH) do
+          system(%(git clone --branch gh-pages #{remote} #{DOC_PATH}))
+        end
+      end
+    end
+
+    desc 'Clone, generate and publish YARD Documentation to GitHub pages'
+    task :publish do
+      Rake::Task["docs:ghpages:clone"].invoke
+      Rake::Task["docs:generate"].invoke
+
       describe = %x(git describe).chomp
       stats    = %x(bundle exec yard stats).chomp
 
