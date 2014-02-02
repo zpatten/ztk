8116d149531cf21e570bfe00f6875b41a8a88943
Zachary Patten <zachary@jovelabs.com>
2014-02-01 20:33:47 -0800

    logger: chomp the message

--- lib/ztk/logger.rb
+++ lib/ztk/logger.rb
@@ -150,7 +150,7 @@ module ZTK
 
       called_by = parse_caller(caller[1+shift])
 
-      message = [message, progname].flatten.compact.join(": ")
+      message = [message.chomp, progname].flatten.compact.join(": ")
       message = "%19s.%06d|%05d|%5s|%s%s\n" % [Time.now.utc.strftime("%Y-%m-%d|%H:%M:%S"), Time.now.utc.usec, Process.pid, SEVERITIES[severity], called_by, message]
 
       @logdev.write(ZTK::ANSI.uncolor(message))
